af173c92a4e8e32d288c811d036ee6d751ec6aeb
Septimus <SeptimusVega@proton.me>
2025-08-02 20:33:32 +0300

    init: We are going Nuxt babyii

---
+++ _nuxt/B76-MQ-O.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,o as c,K as r}from"./Bzt83R0l.js";const s={};function n(e,l){return c(),t("blockquote",null,[r(e.$slots,"default")])}const _=o(s,[["render",n]]);export{_ as default};
